cd6a1d5853719fea53af4862351640c55542b5eb
PrashantMaurya252 <mauryaprashant252@gmail.com>
2024-10-19 22:22:53 +0530

    explore item component created

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,8 +1,23 @@
 import './ExploreMenu.css'
+import { menu_list } from '../../assets/assets'
 
-const ExploreMenu = () => {
+const ExploreMenu = ({category,setCategory}) => {
   return (
-    <div>ExploreMenu</div>
+    <div className='explore-menu' id='explore_menu'>
+      <h1>Explore our menu</h1>
+      <p className='explore-menu-text'>Choose from a diverse menu featuring a delectable array of dishes</p>
+      <div className='explore-menu-list'>
+        {menu_list.map((item,index)=>{
+          return (
+            <div onClick={()=>setCategory(prev => prev === item?.menu_name ? "All" : item?.menu_name)} key={index} className='explore-menu-list-item'>
+              <img className={category === item.menu_name ? "active" : ""} src={item.menu_image} alt="menu-image"/>
+              <p>{item.menu_name}</p>
+            </div>
+          )
+        })}
+      </div>
+      <hr/>
+    </div>
   )
 }
 
