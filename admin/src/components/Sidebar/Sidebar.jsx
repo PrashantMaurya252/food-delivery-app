1ce3feede22ea5c46f4d8a9cc9127a0fe6e6fb8c
PrashantMaurya252 <mauryaprashant252@gmail.com>
2024-10-23 22:19:36 +0530

    built some food controller and setting up admin dashboard

---
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,12 @@
+
+import './Sidebar.css'
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+        <div className="sidebar-options"></div>
+    </div>
+  )
+}
+
+export default Sidebar
