ee9a36ceb8d15dc533f388e128b9ea295cbbdeb5
PrashantMaurya252 <mauryaprashant252@gmail.com>
2024-10-28 13:44:48 +0530

    payment backend setup

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 async function mongoDB(){
     try {
+       
         
         await mongoose.connect(`${process.env.MONGO_URI}`)
         console.log("database connected properly")
--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,35 @@
+
+import dotenv from 'dotenv'
+dotenv.config()
+import userRouter from "./routes/userRoute.js";
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 import express from "express";
 import cors from 'cors'
 import mongoDB from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
-import dotenv from 'dotenv'
-import userRouter from "./routes/userRoute.js";
-import cartRouter from "./routes/cartRoute.js";
 
+
+
+mongoDB()
 const app =express()
 const port = 5000
 
-dotenv.config()
+
 app.use(express.json())
 app.use(cors())
 
 
 
-mongoDB()
+
+
+
 
 app.use("/api/food",foodRouter)
 app.use('/images',express.static('uploads'))
 app.use("/api/user",userRouter)
 app.use("/api/cart",cartRouter)
+app.use("/api/order",orderRouter)
 app.get("/",(req,res)=>{
     res.send("API working")
 })
